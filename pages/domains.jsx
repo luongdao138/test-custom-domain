03640f4f6aa4b8475e0ebbc58f4cd4b7109cec8d
luongdao <luong.dao@sotatek.com>
2024-09-16 14:25:22 +0700

    test

---
+++ pages/domains.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CustomDomainsPage = () => {
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="Enter your domain" />
+      </form>
+    </div>
+  );
+};
+
+export default CustomDomainsPage;
